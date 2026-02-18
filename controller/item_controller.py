b9656c9b444b5fcb73a91d7512918a4e62ad898d
shlomi <shlomi.shakir@gmail.com>
2026-02-18 14:42:40 +0200

    feat: add controller route for find_item_by_id service

--- controller/item_controller.py
+++ controller/item_controller.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from fastapi import APIRouter, HTTPException
 from starlette import status
 from model.item import Item
@@ -20,14 +20,19 @@ async def get_items():
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No items found")
 
 
-# TODO: think if we want an explicit exception like in get users or just []
-
 @router.get("/search", status_code=status.HTTP_200_OK, response_model=List[Item])
 async def filter_items(name: str) -> List[Item]:
     items = await item_service.filter_items(name)
     if len(items) > 0:
         return items
     return []
+
+@router.get("/{item_id}", status_code=status.HTTP_200_OK, response_model=ItemResponse)
+async def get_item(item_id: int) -> Optional[Item]:
+    item = await item_service.get_item_by_id(item_id)
+    if item is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
+    return item
 
 
 
