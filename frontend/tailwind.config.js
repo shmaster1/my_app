c7b37d3debce340fec1b37987a76eacb1dd81a35
shlomi <Shlomi.Shakir@gmail.com>
2026-04-14 08:54:41 +0300

    Add tool tip (#50)
    * add redirect param
    
    * Revert "add redirect param"
    
    This reverts commit 674060306bbc57fa910f1009aa3cb96da55ca323.
    
    * add new ui
    
    * add new ui
    
    * align git ignore
    
    * add chat tool tip for first time login

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   theme: {
     extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0", transform: "translateY(6px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.25s ease-out both",
+      },
       fontFamily: {
         display: ["'Bebas Neue'", "cursive"],
         body: ["'DM Sans'", "sans-serif"],
