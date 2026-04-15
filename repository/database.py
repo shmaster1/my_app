d200750b48b652de49bdd28e5ef38179a9254b07
shlomi <Shlomi.Shakir@gmail.com>
2026-04-15 14:20:35 +0300

    Fix main04 (#89)
    * add railway.toml and Procfile
    
    * fixed main and database

--- repository/database.py
+++ repository/database.py
@@ -1,5 +1,14 @@
-from config.config import Config
+import ssl
 from databases import Database
+from config.config import Config
 
 config = Config()
-database = Database(config.DATABASE_URL)
+
+ssl_ctx = ssl.create_default_context()
+ssl_ctx.check_hostname = False
+ssl_ctx.verify_mode = ssl.CERT_NONE
+
+database = Database(
+    config.DATABASE_URL.replace("?ssl=true", ""),
+    ssl=ssl_ctx
+)
